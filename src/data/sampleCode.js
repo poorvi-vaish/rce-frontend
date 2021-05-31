c16397ed963901600fe2a90d4db7bf5ea706241f
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-05-31 18:06:29 +0530

    refactor: tested this commitzen

--- src/data/sampleCode.js
+++ src/data/sampleCode.js
@@ -1,5 +1,5 @@
 export const SAMPLE_CODE = {
-  javascript: `// Sample Code \n\nconsole.log("Hello World");`,
+  javascript: `// Sample Code \n\nconsole.log("Domain Changed!!!");`,
   python: `# Sample Code \n\nprint("Hello World")`,
   cpp: `// Sample Code \n\n#include<iostream>\nusing namespace std;\n\nint main() {\n\tcout << "Hello World" << endl;\n\treturn 0;\n}`,
 };
